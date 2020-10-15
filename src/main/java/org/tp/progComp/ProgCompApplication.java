5beae811b88f84a87f2f2618df821d23190e146c
Tenalic <43466853+Tenalic@users.noreply.github.com>
2020-10-15 14:43:35 +0200

    Init
    Init

---
+++ src/main/java/org/tp/progComp/ProgCompApplication.java
@@ -0,0 +1,13 @@
+package org.tp.progComp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgCompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgCompApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tp/progComp/ProgCompApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tp.progComp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgCompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
