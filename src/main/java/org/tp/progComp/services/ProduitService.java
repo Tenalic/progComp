a738fd60a5aca4e6707c6b22b95720393bec8209
Tenalic <43466853+Tenalic@users.noreply.github.com>
2020-12-03 12:04:48 +0100

    1.0

--- src/main/java/org/tp/progComp/services/ProduitService.java
+++ src/main/java/org/tp/progComp/services/ProduitService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.tp.progComp.bdd.product.ProductConnectionRepository;
-import org.tp.progComp.entities.Annonce;
 import org.tp.progComp.entities.Produit;
 
 @Service
@@ -14,20 +13,6 @@ public class ProduitService implements ProduitServiceInterface {
 
 	@Autowired
 	private ProductConnectionRepository productRepository;
-	
-	/*@Autowired
-	public Produit findProduit(String name, String categorie,String miniCategorie, String nameVendeur) {
-		return productRepository.findByNomProduitAndCategorieAndPseudoVendeur(name, categorie, nameVendeur);
-	}*/
-	
-	/*@Transactional
-	public Produit createProduit(String name, String categorie, String miniCategorie, String vendorsName) {
-		if (findProduit(name, categorie, miniCategorie, vendorsName) == null) {
-			return productRepository.save(new Produit(name, categorie, miniCategorie, vendorsName));
-		} else {
-			return null;
-		}
-	}*/
 
 	@Transactional
 	public Produit createProduit2(String name, String categorie, String miniCategorie, String vendorsName) {
